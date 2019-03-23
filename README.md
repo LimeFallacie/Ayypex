be4813268a61445178abc2dc59e4985c079e2f22
LimeFallacie <e0014667@u.nus.edu>
2019-03-23 20:11:50 +0800

    some OneMap calls & minor function declarations

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Ayypex
-git repo for ayypex, whatever the fuck we doing.
+Ayypex Carpark sniffer
+
+This program aims to take in a user address (most likely postal code);
+and returns the nearest available carparks with open lots.
+
+Program flow:
+
+1) User Input (postal code) is converted to SVY21 format (OneMap REST API)
+
+2) Use User_SVY21_location to locate nearest carparks (HDB Carpark Information)
+
+3) Check for available lots in listed nearby carparks (data.gov carpark availability)
+
+4) Generate route maps (currently PNG) for each of the shortlisted nearby carparks with available lots
+
+APIs used:
+
+- OneMap
+- HDB Carpark Information 
+- Carpark Availability
--- example.py
+++ example.py
@@ -1,5 +1,32 @@
 #this code snippet queries HDB carpark information
 import requests
-url = 'https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c&limit=5'
-print(requests.get(url).json())
+
+ONEMAP_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI1MTksInVzZXJfaWQiOjI1MTksImVtYWlsIjoiYm9rd29vbi5jQGdtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC9vbTIuZGZlLm9uZW1hcC5zZ1wvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTU1MzM0MTc4NywiZXhwIjoxNTUzNzczNzg3LCJuYmYiOjE1NTMzNDE3ODcsImp0aSI6IjU2ZWI1YmRkZjBjMDkwNWNkZWNmMWNkYzBmNTViYjY0In0.r7M2SMKECiSdMdSNcuqO8JIQcV9JeE4YIwe56pIW_iA"
+
+def retrieve_carpark_list():
+    url = 'https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c&limit=5'
+    Jlist = requests.get(url).json()
+
+def user_location():
+    postal_code = input()
+
+
+#def find_nearest():
+
+
+
+
+def convert_svy_to_wgs(): #for inputting params for routing
+    url = 'https://developers.onemap.sg/commonapi/convert/3414to4326?'
+    params = 'X=28983.788791079794&Y=33554.5098132845'
+
+    print(requests.get(url + params).json())
+
+def main():
+    user_location()
+    convert_svy_to_wgs()
+    #find_nearest(user_location())
+
+
+main()
 
